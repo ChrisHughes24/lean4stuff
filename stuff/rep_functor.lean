0561ebe43e9115e064f0ad9d91de3eb996518dfc
ChrisHughes24 <chrishughes24@gmail.com>
2022-06-30 14:41:40 +0200

    Struggling with induction

--- stuff/rep_functor.lean
+++ stuff/rep_functor.lean
@@ -47,109 +47,215 @@ structure Context : Type 1 :=
 
 variable (Γ : Context)
 
-inductive Elem : (X : Obj Cat.Set) → Type
-| var (X : Obj Cat.Set) (x : Γ.elem X) : Elem X
-| id {C : Cat} (X : Obj C) : Elem ((Func.hom X).app X)
-| comp {C : Cat} {X Y Z : Obj C}
-  (f : Elem ((hom X).app Y))
-  (g : Elem ((hom Y).app Z)) : Elem ((hom X).app Z)
-| app {X Y : Obj Cat.Set} (f : Elem ((hom X).app Y)) (x : Elem X) : Elem Y
-| map {C D : Cat} (F : Func C D) {X Y : Obj C} (f : Elem ((hom X).app Y)) :
-  Elem (app (hom (app F X)) (app F Y))
-| extend {C : Cat} (F : Func C Cat.Set) (h : Γ.isCorepr F) {X : Obj C} :
-  Elem ((app (hom (app F X)) (app (hom (corepr F)) X)))
-| unit {C : Cat} (F : Func C Cat.Set)  (h : Γ.isCorepr F) :
-  Elem (app F (corepr F))
--- | prod_mk {C : Cat} (F G : Func C Cat.Set) (X : Obj C) :
---   Elem (app (hom (app F X)) (app (hom (app G X)) (app (prod F G) X)))
-| prodMk {C : Cat} {F G : Func C Cat.Set} {X : Obj C}
-  (x : Elem (F.app X)) (y : Elem (G.app X)) :
-  Elem ((F.prod G).app X)
-| prodFst {C : Cat} {F G : Func C Cat.Set} {X : Obj C}
-  (x : Elem ((F.prod G).app X)) : Elem (F.app X)
-| prodSnd {C : Cat} {F G : Func C Cat.Set} {X : Obj C}
-  (x : Elem ((F.prod G).app X)) : Elem (G.app X)
+-- inductive Elem : (X : Obj Cat.Set) → Type
+-- | var (X : Obj Cat.Set) (x : Γ.elem X) : Elem X
+-- | id {C : Cat} (X : Obj C) : Elem ((Func.hom X).app X)
+-- | comp {C : Cat} {X Y Z : Obj C}
+--   (f : Elem ((hom X).app Y))
+--   (g : Elem ((hom Y).app Z)) : Elem ((hom X).app Z)
+-- | app {X Y : Obj Cat.Set} (f : Elem ((hom X).app Y)) (x : Elem X) : Elem Y
+-- | map {C D : Cat} (F : Func C D) {X Y : Obj C} (f : Elem ((hom X).app Y)) :
+--   Elem (app (hom (app F X)) (app F Y))
+-- | extend {C : Cat} (F : Func C Cat.Set) (h : Γ.isCorepr F) {X : Obj C} :
+--   Elem ((app (hom (app F X)) (app (hom (corepr F)) X)))
+-- | unit {C : Cat} (F : Func C Cat.Set)  (h : Γ.isCorepr F) :
+--   Elem (app F (corepr F))
+-- | prodMk {C : Cat} {F G : Func C Cat.Set} {X : Obj C}
+--   (x : Elem (F.app X)) (y : Elem (G.app X)) :
+--   Elem ((F.prod G).app X)
+-- | prodFst {C : Cat} {F G : Func C Cat.Set} {X : Obj C}
+--   (x : Elem ((F.prod G).app X)) : Elem (F.app X)
+-- | prodSnd {C : Cat} {F G : Func C Cat.Set} {X : Obj C}
+--   (x : Elem ((F.prod G).app X)) : Elem (G.app X)
 
-inductive Rel : {X : Obj Cat.Set} → (x y : Elem Γ X) → Prop
-| refl {X : Obj Cat.Set} (x : Elem Γ X) : Rel x x
-| symm {X : Obj Cat.Set} {x y : Elem Γ X} : Rel x y → Rel y x
-| trans {X : Obj Cat.Set} {x y z : Elem Γ X} : Rel x y → Rel y z → Rel x z
-| idComp {C : Cat} {X Y : Obj C} {f : Elem Γ ((hom X).app Y)} :
-  Rel ((Elem.id X).comp f) f
-| compId {C : Cat} {X Y : Obj C} {f : Elem Γ ((hom X).app Y)} :
-  Rel (f.comp (Elem.id Y)) f
-| compAssoc {C : Cat} {W X Y Z : Obj C} (f : Elem Γ ((hom W).app X))
-  (g : Elem Γ ((hom X).app Y)) (h : Elem Γ ((hom Y).app Z)) :
-  Rel ((f.comp g).comp h) (f.comp (g.comp h))
-| idApp {X : Obj Cat.Set} (x : Elem Γ X) :
-  Rel ((Elem.id X).app x) x
-| compApp {X Y Z : Obj Cat.Set} (f : Elem Γ ((hom X).app Y))
-  (g : Elem Γ ((hom Y).app Z)) (x : Elem Γ X):
-  Rel ((f.comp g).app x) (g.app (f.app x))
-| mapId {C D : Cat} (F : Func C D) (X : Obj C) :
-  Rel (Elem.map F (Elem.id X)) (Elem.id (F.app X))
-| mapComp {C D : Cat} (F : Func C D) {X Y Z : Obj C}
-  (f : Elem Γ ((hom X).app Y)) (g : Elem Γ ((hom Y).app Z)) :
-  Rel (Elem.map F (f.comp g)) ((Elem.map F f).comp (Elem.map F g))
-| fstMk {C : Cat} (F G : Func C Cat.Set){X : Obj C}
-  (x : Elem Γ (F.app X)) (y : Elem Γ (G.app X)) :
-  Rel (Elem.prodFst (x.prodMk y)) x
-| sndMk {C : Cat} (F G : Func C Cat.Set){X : Obj C}
-  (x : Elem Γ (F.app X)) (y : Elem Γ (G.app X)) :
-  Rel (Elem.prodSnd (x.prodMk y)) y
-| compCongr {C : Cat} {X Y Z : Obj Cat.Set} {f₁ f₂ : Elem Γ ((hom X).app Y)}
-  {g₁ g₂ : Elem Γ ((hom Y).app Z)} :
-  Rel f₁ f₂ → Rel g₁ g₂ → Rel (f₁.comp g₁) (f₂.comp g₂)
-| appCongr {X Y : Obj Cat.Set} {f₁ f₂ : Elem Γ ((hom X).app Y)} {x₁ x₂ : Elem Γ X} :
-  Rel f₁ f₂ → Rel x₁ x₂ → Rel (f₁.app x₁) (f₂.app x₂)
-| mapCongr {C D : Cat} {F : Func C D} {X Y : Obj C} {f₁ f₂ : Elem Γ ((hom X).app Y)} :
-  Rel f₁ f₂ → Rel (Elem.map F f₁) (Elem.map F f₂)
-| prodMkCongr {C : Cat} (F G : Func C Cat.Set) {X : Obj C}
-  {x₁ x₂ : Elem Γ (F.app X)} {y₁ y₂ : Elem Γ (G.app X)} :
-  Rel x₁ x₂ → Rel y₁ y₂ → Rel (x₁.prodMk y₁) (x₂.prodMk y₂)
-| prodFstCongr {C : Cat} {F G : Func C Cat.Set} {X : Obj C}
-  (x₁ x₂ : Elem Γ ((F.prod G).app X)) :
-  Rel x₁ x₂ → Rel x₁.prodFst x₂.prodFst
-| prodSndCongr {C : Cat} {F G : Func C Cat.Set} {X : Obj C}
-  (x₁ x₂ : Elem Γ ((F.prod G).app X)) :
-  Rel x₁ x₂ → Rel x₁.prodSnd x₂.prodSnd
+-- inductive Rel : {X : Obj Cat.Set} → (x y : Elem Γ X) → Prop
+-- | refl {X : Obj Cat.Set} (x : Elem Γ X) : Rel x x
+-- | symm {X : Obj Cat.Set} {x y : Elem Γ X} : Rel x y → Rel y x
+-- | trans {X : Obj Cat.Set} {x y z : Elem Γ X} : Rel x y → Rel y z → Rel x z
+-- | idComp {C : Cat} {X Y : Obj C} {f : Elem Γ ((hom X).app Y)} :
+--   Rel ((Elem.id X).comp f) f
+-- | compId {C : Cat} {X Y : Obj C} {f : Elem Γ ((hom X).app Y)} :
+--   Rel (f.comp (Elem.id Y)) f
+-- | compAssoc {C : Cat} {W X Y Z : Obj C} (f : Elem Γ ((hom W).app X))
+--   (g : Elem Γ ((hom X).app Y)) (h : Elem Γ ((hom Y).app Z)) :
+--   Rel ((f.comp g).comp h) (f.comp (g.comp h))
+-- | idApp {X : Obj Cat.Set} (x : Elem Γ X) :
+--   Rel ((Elem.id X).app x) x
+-- | compApp {X Y Z : Obj Cat.Set} (f : Elem Γ ((hom X).app Y))
+--   (g : Elem Γ ((hom Y).app Z)) (x : Elem Γ X):
+--   Rel ((f.comp g).app x) (g.app (f.app x))
+-- | mapId {C D : Cat} (F : Func C D) (X : Obj C) :
+--   Rel (Elem.map F (Elem.id X)) (Elem.id (F.app X))
+-- | mapComp {C D : Cat} (F : Func C D) {X Y Z : Obj C}
+--   (f : Elem Γ ((hom X).app Y)) (g : Elem Γ ((hom Y).app Z)) :
+--   Rel (Elem.map F (f.comp g)) ((Elem.map F f).comp (Elem.map F g))
+-- | fstMk {C : Cat} (F G : Func C Cat.Set){X : Obj C}
+--   (x : Elem Γ (F.app X)) (y : Elem Γ (G.app X)) :
+--   Rel (Elem.prodFst (x.prodMk y)) x
+-- | sndMk {C : Cat} (F G : Func C Cat.Set){X : Obj C}
+--   (x : Elem Γ (F.app X)) (y : Elem Γ (G.app X)) :
+--   Rel (Elem.prodSnd (x.prodMk y)) y
+-- | compCongr {C : Cat} {X Y Z : Obj Cat.Set} {f₁ f₂ : Elem Γ ((hom X).app Y)}
+--   {g₁ g₂ : Elem Γ ((hom Y).app Z)} :
+--   Rel f₁ f₂ → Rel g₁ g₂ → Rel (f₁.comp g₁) (f₂.comp g₂)
+-- | appCongr {X Y : Obj Cat.Set} {f₁ f₂ : Elem Γ ((hom X).app Y)} {x₁ x₂ : Elem Γ X} :
+--   Rel f₁ f₂ → Rel x₁ x₂ → Rel (f₁.app x₁) (f₂.app x₂)
+-- | mapCongr {C D : Cat} {F : Func C D} {X Y : Obj C} {f₁ f₂ : Elem Γ ((hom X).app Y)} :
+--   Rel f₁ f₂ → Rel (Elem.map F f₁) (Elem.map F f₂)
+-- | prodMkCongr {C : Cat} (F G : Func C Cat.Set) {X : Obj C}
+--   {x₁ x₂ : Elem Γ (F.app X)} {y₁ y₂ : Elem Γ (G.app X)} :
+--   Rel x₁ x₂ → Rel y₁ y₂ → Rel (x₁.prodMk y₁) (x₂.prodMk y₂)
+-- | prodFstCongr {C : Cat} {F G : Func C Cat.Set} {X : Obj C}
+--   (x₁ x₂ : Elem Γ ((F.prod G).app X)) :
+--   Rel x₁ x₂ → Rel x₁.prodFst x₂.prodFst
+-- | prodSndCongr {C : Cat} {F G : Func C Cat.Set} {X : Obj C}
+--   (x₁ x₂ : Elem Γ ((F.prod G).app X)) :
+--   Rel x₁ x₂ → Rel x₁.prodSnd x₂.prodSnd
+
+inductive HeadSymbol
+| VEU : HeadSymbol -- var, extend or unit
+| Fst : HeadSymbol
+| Snd : HeadSymbol
+| ProdMk : HeadSymbol
+| IC : HeadSymbol -- id, comp
+| AM : HeadSymbol -- app, map
+
+open HeadSymbol
+
+inductive NormElemAux : HeadSymbol → Obj Cat.Set →  Type
+| var (X : Obj Cat.Set) (x : Γ.elem X) : NormElemAux VEU X
+| id {C : Cat} (X : Obj C) : NormElemAux ICAM ((Func.hom X).app X)
+| comp {C : Cat} {X Y Z : Obj C} {s t : HeadSymbol} (hs : s ≠ IC)
+  (f : NormElemAux s ((hom X).app Y))
+  (g : NormElemAux t ((hom Y).app Z)) : NormElemAux IC ((hom X).app Z)
+| app {X Y : Obj Cat.Set} {s t : HeadSymbol} (hs : s ≠ IC) (f : NormElemAux s ((hom X).app Y))
+  (x : NormElemAux t X) : NormElemAux AM Y
+| map {C D : Cat} (F : Func C D) {X Y : Obj C} {s : HeadSymbol} (hs : s ≠ IC)
+  (f : NormElemAux s ((hom X).app Y)) :
+  NormElemAux AM (app (hom (app F X)) (app F Y))
+| extend {C : Cat} (F : Func C Cat.Set) (h : Γ.isCorepr F) {X : Obj C} :
+  NormElemAux VEU ((app (hom (app F X)) (app (hom (corepr F)) X)))
+| unit {C : Cat} (F : Func C Cat.Set)  (h : Γ.isCorepr F) :
+  NormElemAux VEU (app F (corepr F))
+| prodMk {C : Cat} {F G : Func C Cat.Set} {X : Obj C} {s t : HeadSymbol}
+  (hst : s ≠ Fst ∨ t ≠ Snd)
+  (x : NormElemAux s (F.app X)) (y : NormElemAux t (G.app X)) :
+  NormElemAux ProdMk ((F.prod G).app X)
+| prodFst {C : Cat} {F G : Func C Cat.Set} {X : Obj C} {s : HeadSymbol}
+  (hs : s ≠ ProdMk)
+  (x : NormElemAux s ((F.prod G).app X)) : NormElemAux Fst (F.app X)
+| prodSnd {C : Cat} {F G : Func C Cat.Set} {X : Obj C} {s : HeadSymbol}
+  (hs : s ≠ ProdMk)
+  (x : NormElemAux s ((F.prod G).app X)) : NormElemAux Snd (G.app X)
+
+def NormElem (X : Obj Cat.Set) : Type :=
+(s : HeadSymbol) × NormElemAux Γ s X
 
 namespace NormElem
 
-mutual
+variable {C : Cat}
 
--- prodMk can only be applied to terms not of the form fst snd.
+def var (X : Obj Cat.Set) (v : Γ.elem X) : NormElem Γ X :=
+⟨VEU, NormElemAux.var X v⟩
 
--- We have raw Elems and Elems. Elems are the closure under identity
--- and composition functor map and function application of raw Elems.
--- We can only apply or map raw homs.
+def id (X : Obj C) : NormElem Γ ((hom X).app X) :=
+⟨IC, NormElemAux.id X⟩
 
-inductive RawElem : Obj Cat.Set → Type where
-| var (X : Obj Cat.Set) (x : Γ.elem X) : RawElem X
-| extend {C : Cat} (F : Func C Cat.Set) (h : Γ.isCorepr F) {X : Obj C} :
-  RawElem ((app (hom (app F X)) (app (hom (corepr F)) X)))
-| unit {C : Cat} (F : Func C Cat.Set)  (h : Γ.isCorepr F) :
-  RawElem (app F (corepr F))
+def comp {X Y Z : Obj C}
+  {s : HeadSymbol}
+  (x : NormElemAux Γ s ((hom X).app Y)) :
+  {t : HeadSymbol} →
+  (y : NormElemAux Γ t ((hom Y).app Z)) →
+  NormElem Γ ((hom X).app Z) :=
+NormElemAux.recOn x _ _ _ _ _ _ _ _ _ _
 
-inductive RawElemFst : Obj Cat.Set → Type where
-| ofVar {C : Cat} {F G : Func C Cat.Set} {X : Obj C} (x : Γ.elem ((F.prod G).app X)) :
-  RawElemFst (F.app X)
-| ofRawElemSnd {C : Cat} {F G : Func C Cat.Set} {X : Obj C}
-  (x : RawElemSnd ((F.prod G).app X)) : RawElemFst (F.app X)
 
-inductive RawElemSnd : Obj Cat.Set → Type where
-| ofVar {C : Cat} {F G : Func C Cat.Set} {X : Obj C} (x : Γ.elem ((F.prod G).app X)) :
-  RawElemSnd (G.app X)
-| ofRawElemFst {C : Cat} {F G : Func C Cat.Set} {X : Obj C}
-  (x : RawElemFst ((F.prod G).app X)) : RawElemSnd (G.app X)
-
-inductive RawElemProdMk : Obj Cat.Set → Type where
-| ofFstLeft :
-
-inductive Elem : Obj Cat.Set → Type where
-| id {C : Cat} (X : Obj C) : Elem ((Func.hom X).app X)
-|
-
-end
 
 end NormElem
+
+-- mutual
+
+
+
+-- -- prodMk can only be applied to terms not of the form fst snd.
+
+-- -- We have raw Elems and Elems. Elems are the closure under identity
+-- -- and composition functor map and function application of raw Elems.
+-- -- We can only apply or map raw homs.
+
+-- inductive RawElem : Obj Cat.Set → Type where
+-- | var (X : Obj Cat.Set) (x : Γ.elem X) : RawElem X
+-- | extend {C : Cat} (F : Func C Cat.Set) (h : Γ.isCorepr F) {X : Obj C} :
+--   RawElem ((app (hom (app F X)) (app (hom (corepr F)) X)))
+-- | unit {C : Cat} (F : Func C Cat.Set)  (h : Γ.isCorepr F) :
+--   RawElem (app F (corepr F))
+
+-- inductive RawElemFst : Obj Cat.Set → Type where
+-- | ofVar {C : Cat} {F G : Func C Cat.Set} {X : Obj C} (x : Γ.elem ((F.prod G).app X)) :
+--   RawElemFst (F.app X)
+-- | ofRawElemSnd {C : Cat} {F G : Func C Cat.Set} {X : Obj C}
+--   (x : RawElemSnd ((F.prod G).app X)) : RawElemFst (F.app X)
+-- | ofCompMapAppElem {C : Cat} {F G : Func C Cat.Set} {X : Obj C}
+--   (x : CompMapAppElem ((F.prod G).app X)) : RawElemFst (F.app X)
+
+-- inductive RawElemSnd : Obj Cat.Set → Type where
+-- | ofVar {C : Cat} {F G : Func C Cat.Set} {X : Obj C} (x : Γ.elem ((F.prod G).app X)) :
+--   RawElemSnd (G.app X)
+-- | ofRawElemFst {C : Cat} {F G : Func C Cat.Set} {X : Obj C}
+--   (x : RawElemFst ((F.prod G).app X)) : RawElemSnd (G.app X)
+-- | ofCompMapAppElem {C : Cat} {F G : Func C Cat.Set} {X : Obj C}
+--   (x : CompMapAppElem ((F.prod G).app X)) : RawElemFst (G.app X)
+
+-- inductive RawElemProdMk : Obj Cat.Set → Type where
+-- | ofRawElemRawElem {C : Cat} {F G : Func C Cat.Set} {X : Obj C}
+--   (x : RawElem (F.app X)) (y : RawElem (G.app X)) :
+--   RawElemProdMk ((F.prod G).app X)
+-- | ofRawElemRawElemFst {C : Cat} {F G : Func C Cat.Set} {X : Obj C}
+--   (x : RawElem (F.app X)) (y : RawElemFst (G.app X)) :
+--   RawElemProdMk ((F.prod G).app X)
+-- | ofRawElemRawElemSnd {C : Cat} {F G : Func C Cat.Set} {X : Obj C}
+--   (x : RawElem (F.app X)) (y : RawElemSnd (G.app X)) :
+--   RawElemProdMk ((F.prod G).app X)
+-- | ofRawElemRawElemProdMk {C : Cat} {F G : Func C Cat.Set} {X : Obj C}
+--   (x : RawElem (F.app X)) (y : RawElemProdMk (G.app X)) :
+--   RawElemProdMk ((F.prod G).app X)
+-- | ofRawElemElem {C : Cat} {F G : Func C Cat.Set} {X : Obj C}
+--   (x : RawElem (F.app X)) (y : Elem (G.app X)) :
+--   RawElemProdMk ((F.prod G).app X)
+
+-- | ofRawElemFstRawElem {C : Cat} {F G : Func C Cat.Set} {X : Obj C}
+--   (x : RawElemFst (F.app X)) (y : RawElem (G.app X)) :
+--   RawElemProdMk ((F.prod G).app X)
+-- | ofRawElemFstRawElemFst {C : Cat} {F G : Func C Cat.Set} {X : Obj C}
+--   (x : RawElemFst (F.app X)) (y : RawElemFst (G.app X)) :
+--   RawElemProdMk ((F.prod G).app X)
+-- | ofRawElemFstRawElemProdMk {C : Cat} {F G : Func C Cat.Set} {X : Obj C}
+--   (x : RawElemFst (F.app X)) (y : RawElemProdMk (G.app X)) :
+--   RawElemProdMk ((F.prod G).app X)
+-- | ofRawElemFstElem {C : Cat} {F G : Func C Cat.Set} {X : Obj C}
+--   (x : RawElemFst (F.app X)) (y : Elem (G.app X)) :
+--   RawElemProdMk ((F.prod G).app X)
+
+-- | ofRawElemSndRawElem {C : Cat} {F G : Func C Cat.Set} {X : Obj C}
+--   (x : RawElemSnd (F.app X)) (y : RawElem (G.app X)) :
+--   RawElemProdMk ((F.prod G).app X)
+-- | ofRawElemRawElemFst {C : Cat} {F G : Func C Cat.Set} {X : Obj C}
+--   (x : RawElemSnd (F.app X)) (y : RawElemFst (G.app X)) :
+--   RawElemProdMk ((F.prod G).app X)
+-- | ofRawElemRawElemSnd {C : Cat} {F G : Func C Cat.Set} {X : Obj C}
+--   (x : RawElemSnd (F.app X)) (y : RawElemSnd (G.app X)) :
+--   RawElemProdMk ((F.prod G).app X)
+-- | ofRawElemRawElemProdMk {C : Cat} {F G : Func C Cat.Set} {X : Obj C}
+--   (x : RawElemSnd (F.app X)) (y : RawElemProdMk (G.app X)) :
+--   RawElemProdMk ((F.prod G).app X)
+-- | ofRawElemElem {C : Cat} {F G : Func C Cat.Set} {X : Obj C}
+--   (x : RawElemSnd (F.app X)) (y : Elem (G.app X)) :
+--   RawElemProdMk ((F.prod G).app X)
+
+-- inductive CompMapAppElem : Obj Cat.Set → Type where
+-- | id {C : Cat} (X : Obj C) : Elem ((Func.hom X).app X)
+-- |
+
+-- end
+
+-- end NormElem
