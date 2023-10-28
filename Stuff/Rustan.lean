d96140754847da5d23987aa9ad7c74dd3ad45d28
Chris Hughes <chrishughes24@gmail.com>
2023-10-28 09:56:28 +0100

    golf

--- Stuff/Rustan.lean
+++ Stuff/Rustan.lean
@@ -55,19 +55,21 @@ theorem E.eval_ite_ite (a b c d e : E) (f : ℕ → Bool) :
 
 attribute [local simp] eval normalized hasNestedIf hasConstantIf hasRedundantIf
   disjoint vars List.disjoint max_add_add_right max_mul_mul_left
+  Nat.lt_add_one_iff le_add_of_le_right
 
 @[simp]
 def E.normSize : E → ℕ
   | lit _ => 0
-  | var _ => 0
-  | .ite i t e => 2 * normSize i + max (normSize t) (normSize e) + 1
+  | var _ => 1
+  | .ite i t e => 2 * normSize i +  max (normSize t) (normSize e) + 1
 
 /-- Normalizes the expression at the same time as assigning all variables in
 `l` to the literal boolean given by `l` -/
 def E.normalize (l : AList (fun _ : ℕ => Bool)) :
-    (e : E) → { e' : E // e'.normalized ∧
+    (e : E) → { e' : E //
         (∀ f, e'.eval f = e.eval
           (fun w => (l.lookup w).elim (f w) (fun b => b))) ∧
+        e'.normalized ∧
         ∀ (v : ℕ) (b : Bool), v ∈ vars e' → l.lookup v ≠ some b }
   | lit b => ⟨lit b, by simp⟩
   | var v =>
@@ -81,80 +83,52 @@ def E.normalize (l : AList (fun _ : ℕ => Bool)) :
     have ⟨e', he'⟩ := E.normalize l e
     ⟨e', by aesop⟩
   | .ite (.ite a b c) d e =>
-    have ⟨t', ht₁, ht₂, ht₃⟩ := E.normalize l (.ite a (.ite b d e) (.ite c d e))
-    ⟨t', ht₁, fun f => by rw [ht₂, eval_ite_ite], ht₃⟩
+    have ⟨t', ht₁, ht₂⟩ := E.normalize l (.ite a (.ite b d e) (.ite c d e))
+    ⟨t', fun f => by rw [ht₁, eval_ite_ite], ht₂⟩
   | .ite (var v) t e =>
     match h : l.lookup v with
     | none =>
       have ⟨t', ht₁, ht₂, ht₃⟩ := E.normalize (l.insert v true) t
       have ⟨e', he₁, he₂, he₃⟩ := E.normalize (l.insert v false) e
-      if hte' : t' = e'
-      then by
-        subst hte'
-        refine ⟨t', ht₁, ?_, ?_⟩
-        · intro f
-          cases hfv : f v
-          · simp only [he₂, Option.elim, ne_eq, eval, h, hfv, cond_false]
-            congr
-            ext w
-            by_cases hwv : w = v
-            · subst w
-              simp [hfv, h]
-            · simp [hwv]
-          · simp only [ht₂, Option.elim, ne_eq, eval, h, hfv, cond_false]
-            congr
-            ext w
-            by_cases hwv : w = v
-            · subst w
-              simp [hfv, h]
-            · simp [hwv]
-        · intro w b
-          by_cases hwv : w = v
-          · subst v
-            simp [h]
-          · have := ht₃ w b
-            have := he₃ w b
-            aesop
-      else ⟨.ite (var v) t' e', by
+      ⟨if t' = e' then t' else .ite (var v) t' e', by
         refine ⟨?_, ?_, ?_⟩
+        · intro f
+          simp only [eval, apply_ite (eval f), ite_eq_iff']
+          cases hfv : f v
+          · simp (config := {contextual := true}) only
+              [cond_false, h, Option.elim, he₁]
+            refine ⟨fun _ => ?_, fun _ => ?_⟩ <;>
+            { congr
+              ext w
+              by_cases hwv : w = v
+              · subst w
+                simp [hfv, h]
+              · simp [hwv] }
+          · simp only [cond_true, h, Option.elim, ht₁]
+            refine ⟨fun _ => ?_, fun _ => ?_⟩ <;>
+            { congr
+              ext w
+              by_cases hwv : w = v
+              · subst w
+                simp [hfv, h]
+              · simp [hwv] }
         · have := ht₃ v true
           have := he₃ v false
           aesop
-        · intro f
-          simp only [eval, ht₂, Option.elim, he₂]
-          cases hfv : f v
-          · simp only [cond_false, h]
-            congr
-            ext w
-            by_cases hwv : w = v
-            · subst w
-              simp [hfv, h]
-            · simp [hwv]
-          · simp only [cond_true, h]
-            congr
-            ext w
-            by_cases hwv : w = v
-            · subst w
-              simp [hfv, h]
-            · simp [hwv]
-        · simp only [vars, List.mem_append, List.mem_singleton]
-          intro w b
+        · intro w b
+          have := ht₃ w b
+          have := he₃ w b
           by_cases hwv : w = v
           · subst v
             simp [h]
-          · have := ht₃ w b
-            have := he₃ w b
-            aesop⟩
-    | some true =>
-      have ⟨t', ht'⟩ := E.normalize l t
-      ⟨t', by aesop⟩
-    | some false =>
-      have ⟨e', he'⟩ := E.normalize l e
-      ⟨e', by aesop⟩
+          · aesop⟩
+    | some b =>
+      have ⟨e', he₁, he₂⟩ := E.normalize l (.ite (lit b) t e)
+      ⟨e', by aesop, he₂⟩
   termination_by E.normalize e => e.normSize
 
 def IfNormalization : Type := { Z : E → E // ∀ e, (Z e).normalized ∧ ∀ f, (Z e).eval f = e.eval f }
 
 example : IfNormalization :=
   ⟨fun e => (E.normalize ∅ e).1,
-   fun e => ⟨(E.normalize ∅ e).2.1, fun f => by simp [(E.normalize ∅ e).2.2.1 f]⟩⟩
+   fun e => ⟨(E.normalize ∅ e).2.2.1, fun f => by simp [(E.normalize ∅ e).2.1 f]⟩⟩
