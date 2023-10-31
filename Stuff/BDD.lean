35c32b86e7365d259f3d42c92bd7c30f291f7ccf
Chris Hughes <chrishughes24@gmail.com>
2023-10-31 14:12:02 +0000

    more work on BDD

--- Stuff/BDD.lean
+++ Stuff/BDD.lean
@@ -5,20 +5,26 @@ inductive BDD (level : ℕ) : Type
   | ite : (var : ℕ) → BDD level → BDD level → BDD level
 deriving Repr, DecidableEq
 
+@[simp]
 def normalized : ∀ {level : ℕ}, BDD level → Bool
   | _, .lit _ => true
-  | _, .ite _ t e => normalized t ∧ normalized e ∧ t ≠ e
+  | _, .ite _ t e => normalized t && normalized e && !t == e
 
 namespace BDD
 
+@[simp]
 def eval {level : ℕ} : BDD level → (ℕ → Bool) → Bool
   | lit b, _ => b
   | .ite var t e, f => if f (var + level) then eval t f else eval e f
+
+
 
 theorem eval_injective : ∀ {level : ℕ} {b₁ b₂ : BDD level},
     normalized b₁ → normalized b₂ → (∀ (f : ℕ → Bool), eval b₁ f = eval b₂ f) → b₁ = b₂
   | _, .lit b₁, .lit b₂, _, _, hf => by
     simpa using hf (λ _ => false)
+  | _, .lit b, .ite i t e, h₁, h₂, hf => by
+    simp at hf
 
 
 
